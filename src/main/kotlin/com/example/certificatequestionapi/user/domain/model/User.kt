87e1a9115840c08ec4d18d75d9a3a5182e2790c5
motojate <motojate@naver.com>
2024-07-27 23:27:26 +0900

    [FEAT] user 모델 칼럼 추가

--- src/main/kotlin/com/example/certificatequestionapi/user/domain/model/User.kt
+++ src/main/kotlin/com/example/certificatequestionapi/user/domain/model/User.kt
@@ -2,15 +2,19 @@ package com.example.certificatequestionapi.user.domain.model
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
 class User(
-    @Id
+    @Id @Column(name = "user_seq")
     val userSeq: String,
 
-    @Column
-    val name: String
-)
+    @Column(name = "name")
+    val name: String,
+) {
+    @Column(name = "level")
+    val level: Int = 1
+
+    @Column(name = "exp")
+    val exp: Int = 0
+}
