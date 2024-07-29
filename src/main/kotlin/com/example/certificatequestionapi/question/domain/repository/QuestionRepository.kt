13cc7a18e45013eee511953424cdb959aaf45241
motojate <motojate@naver.com>
2024-07-29 22:10:26 +0900

    [FEAT] KotlinJdslJpqlExecutor를 impl에서 주입 받아서 사용하기 때문에 해당 내용 삭제 처리

--- src/main/kotlin/com/example/certificatequestionapi/question/domain/repository/QuestionRepository.kt
+++ src/main/kotlin/com/example/certificatequestionapi/question/domain/repository/QuestionRepository.kt
@@ -1,9 +1,8 @@
 package com.example.certificatequestionapi.question.domain.repository
 
 import com.example.certificatequestionapi.question.domain.model.Question
-import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface QuestionRepository : JpaRepository<Question, Long>, CustomQuestionRepository, KotlinJdslJpqlExecutor
+interface QuestionRepository : JpaRepository<Question, Long>, CustomQuestionRepository
