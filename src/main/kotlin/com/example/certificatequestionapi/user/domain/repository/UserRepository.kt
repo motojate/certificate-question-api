a72dfe591a1ee1e148ae8a23979aa8e350a60306
motojate <motojate@naver.com>
2024-07-29 22:10:36 +0900

    [FEAT] 유저 레포지토리 생성

---
+++ src/main/kotlin/com/example/certificatequestionapi/user/domain/repository/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.example.certificatequestionapi.user.domain.repository
+
+import com.example.certificatequestionapi.user.domain.model.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long>
